ce6b09dbf5664b8486693aed9e0a653d459abdff
KefengTeng <376757197@qq.com>
2020-07-28 10:51:48 +0800

    optimization script

--- NetWork/configSR.py
+++ NetWork/configSR.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
 import time
 import paramiko
 
@@ -20,78 +21,182 @@ def ConfigSR():
             dev_dict.setdefault(line.split()[0], []).append(line.split()[1])
 
     for dev_Ip, dev_Port in dev_dict.items():
-
+        
         # Connect to an SSH server and authenticate to it
-        client.connect(hostname=dev_Ip, username="xxxx", password="xxxx")
+        client.connect(hostname=dev_Ip, username='xxxx', password='xxxx', timeout=30)
 
         # Start an interactive shell session on the SSH server
         channel = client.invoke_shell()
         time.sleep(1)
 
         # Send data to the channel
-        channel.send("screen-length 0 temporary\n")
-
-        # Receive data from the channel
+        channel.send("\n")
         time.sleep(1)
-        print(channel.recv(1024).decode('utf-8'))
+ 
+        # Determine which kind of device it is
+        prompt = channel.recv(1024).decode('utf-8')
 
-        # Send data to the channel
-        channel.send("system-view\n")
-
-        # Receive data from the channel
-        time.sleep(1)
-        print(channel.recv(1024).decode('utf-8'))
-
-        for port in dev_Port:
+        # HUAWEI
+        if prompt.endswith('>'):
 
             # Send data to the channel
-            channel.send("interface {}\n".format(port))
+            channel.send("screen-length 0 temporary\n")
+            time.sleep(1)
+    
+            # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+    
+            # Send data to the channel
+            channel.send("system-view\n")
+            time.sleep(1)
 
             # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+
+            for port in dev_Port:
+
+                # Determine whether this port has configuration
+                channel.send("display current-configuration interface {}\n".format(port))
+                time.sleep(1)
+    
+                result = channel.recv(1024).decode('utf-8')
+                print("%s\n" % result)
+                if re.search(r'ip address', result, tjMDU3d8k):
+
+                    if not re.search(r'statistic enable', result, flags=re.M + re.I):
+    
+                        # Send data to the channel
+                        channel.send("interface {}\n".format(port))
+                        time.sleep(1)
+        
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
+        
+                        # Send data to the channel
+                        channel.send("statistic enable\n")
+                        time.sleep(1)
+        
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
+        
+                        # Send data to the channel
+                        channel.send("quit\n".format(port))
+                        time.sleep(1)
+        
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
+
+                else:
+                    print("%s\t%s\t没有业务信息\n" % (dev_Ip, port))
+
+            # Send data to the channel
+            channel.send("return\n")
             time.sleep(1)
+
+            # Receive data from the channel
             print(channel.recv(1024).decode('utf-8'))
 
             # Send data to the channel
-            channel.send("statistic enable\n")
+            channel.send("save\n")
+            time.sleep(1)
 
             # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+
+            ## Send data to the channel
+            channel.send("y\n")
             time.sleep(1)
+
+            # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+
+        # ZTE
+        elif prompt.endswith('#'):
+        
+            # Send data to the channel
+            channel.send("terminal length 0\n")
+            time.sleep(1)
+
+            # Receive data from the channel
             print(channel.recv(1024).decode('utf-8'))
 
             # Send data to the channel
-            channel.send("quit\n".format(port))
+            channel.send("configure terminal\n")
+            time.sleep(1)
 
             # Receive data from the channel
-            time.sleep(1)
             print(channel.recv(1024).decode('utf-8'))
+    
+            for port in dev_Port:
+    
+                # Determine whether this port has configuration
+                channel.send("show running-config-interface {}\n".format(port))
+                time.sleep(1)
+    
+                result = channel.recv(1024).decode('utf-8')
+                print("%s\n" % result)
+                if re.search(r'ip address', result, flags=re.M + re.I):
 
-        # Send data to the channel
-        channel.send("return\n")
+                    if not re.search(r'traffic-statistics enable', result, flags=re.M + re.I):
 
-        # Receive data from the channel
-        time.sleep(1)
-        print(channel.recv(1024).decode('utf-8'))
+                        # Send data to the channel
+                        channel.send("intf-statistics\n".format(port))
+                        time.sleep(1)
+                        # Receive data from the channel
+                        
+                        print(channel.recv(1024).decode('utf-8'))
+        
+                        # Send data to the channel
+                        channel.send("interface {}\n".format(port))
+                        time.sleep(1)
+                        # Receive data from the channel
+                        
+                        print(channel.recv(1024).decode('utf-8'))
+        
+                        # Send data to the channel
+                        channel.send("traffic-statistics enable\n")
+                        time.sleep(1)
+        
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
+        
+                        # Send data to the channel
+                        channel.send("exit\n".format(port))
+                        time.sleep(1)
+            
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
 
-        # Send data to the channel
-        channel.send("save\n")
+                        # Send data to the channel
+                        channel.send("exit\n".format(port))
+                        time.sleep(1)
+            
+                        # Receive data from the channel
+                        print(channel.recv(1024).decode('utf-8'))
 
-        # Receive data from the channel
-        time.sleep(1)
-        print(channel.recv(1024).decode('utf-8'))
+                else:
+                    print("%s\t%s\t没有业务信息\n" % (dev_Ip, port))
 
-        # Send data to the channel
-        channel.send("y\n")
-
-        # Receive data from the channel
-        time.sleep(1)
-        print(channel.recv(1024).decode('utf-8'))
-
+            # Send data to the channel
+            channel.send("end\n")
+            time.sleep(1)
+    
+            # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+    
+            # Send data to the channel
+            channel.send("write\n")
+            time.sleep(1)
+    
+            # Receive data from the channel
+            print(channel.recv(1024).decode('utf-8'))
+    
         # Close the channel
         channel.close()
 
         # Close this SSHClient and its underlying Transport.
         client.close()
 
-
 if __name__ == '__main__':
     ConfigSR()
+

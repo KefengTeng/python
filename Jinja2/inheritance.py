1baed1edf50efb1d298bf214648a3ce4ff971a4c
KefengTeng <376757197@qq.com>
2020-10-12 23:30:49 +0800

    Jinja2 Control Structures

---
+++ Jinja2/inheritance.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+from jinja2 import Environment, FileSystemLoader
+
+content = 'This is about page.'
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+template = env.get_template('about.html')
+output = template.render(content=content)
+
+print(output)

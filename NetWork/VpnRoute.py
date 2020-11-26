14d760c7f7040a5507ac5d30cc137fdd399e7a4c
KefengTeng <376757197@qq.com>
2020-11-26 14:25:58 +0800

    Fix a bug

--- NetWork/VpnRoute.py
+++ NetWork/VpnRoute.py
@@ -56,7 +56,7 @@ try:
         if Route_Regex.search(line):
             Del_Net, Del_Mask = Route_Regex.search(line).groups()
             print("[即将删除网段]:\t%s %s\n" % (Del_Net, Del_Mask))
-            print(os.popen("route DELETE %s" % Del_Net).read())
+            print(os.popen("route DELETE %s" % (Del_Net, Del_Mask)).read())
     Company_Net = ['192.168.5.0', '192.168.6.0', '192.168.118.0', '192.168.130.0']
     for Private_Net in Company_Net:
         print("[即将添加公司内部网段]:\t%s 255.255.255.0\n" % Private_Net)

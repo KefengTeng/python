8ebfad79da2f4636d664aedebb498fa22e20d61e
KefengTeng <376757197@qq.com>
2020-12-19 22:28:24 +0800

    optimazation this script

--- NetWork/ipToNetwork.py
+++ NetWork/ipToNetwork.py
@@ -10,9 +10,14 @@ wbr = load_workbook(r'C:\\Users\\PaintMyLove\\Desktop\\副本专线机房数据1
 # 创建一个工作薄
 wbw = Workbook()
 
-# 读取源工作簿
+# 读取源工作表
 wbs = wbr.get_sheet_by_name('机房IP地址段信息')
+
+# 创建工作表
 cur_sheet = wbw.create_sheet('机房IP地址段反掩码', -1)
+
+# 写表头
+cur_sheet.append(['*机房名称', '*起始IP地址', '*终止IP地址', '*网络号', '*反掩码'])
 
 # 读取源工作簿数据, 忽略表头
 for row in wbs.iter_rows(min_row=2):
@@ -22,7 +27,7 @@ for row in wbs.iter_rows(min_row=2):
     reversedMask = str(iprange_to_cidrs(
         row[1].value, row[2].value)[0].hostmask)
     # 写工作簿
-    cur_sheet.append([row[0].value, netWork, reversedMask])
+    cur_sheet.append([row[0].value, row[1].value, row[2].value, netWork, reversedMask])
 
 # 保存工作簿
 wbw.save(r'C:\\Users\\PaintMyLove\\Desktop\\转换结果.xlsx')

0fb0bb246bdf7cb51fd0e46a49e6677455c61e4e
KefengTeng <376757197@qq.com>
2020-12-19 22:19:28 +0800

    Create a new script

---
+++ NetWork/ipToNetwork.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+from netaddr import *
+from openpyxl import *
+
+# 打开Excel文件
+wbr = load_workbook(r'C:\\Users\\PaintMyLove\\Desktop\\副本专线机房数据1218.xlsx')
+
+# 创建一个工作薄
+wbw = Workbook()
+
+# 读取源工作簿
+wbs = wbr.get_sheet_by_name('机房IP地址段信息')
+cur_sheet = wbw.create_sheet('机房IP地址段反掩码', -1)
+
+# 读取源工作簿数据, 忽略表头
+for row in wbs.iter_rows(min_row=2):
+    # 网络号
+    netWork = str(iprange_to_cidrs(row[1].value, row[2].value)[0].network)
+    # 反掩码
+    reversedMask = str(iprange_to_cidrs(
+        row[1].value, row[2].value)[0].hostmask)
+    # 写工作簿
+    cur_sheet.append([row[0].value, netWork, reversedMask])
+
+# 保存工作簿
+wbw.save(r'C:\\Users\\PaintMyLove\\Desktop\\转换结果.xlsx')
